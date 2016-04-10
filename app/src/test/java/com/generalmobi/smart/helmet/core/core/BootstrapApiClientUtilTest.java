66bb0eeeb7b0f495d1014a4c78dd570a0e571b79
Subhanshu Shukla <techsupport2@generalmobi.in>
2016-04-10 21:40:43 +0530

    deleted test cases

--- app/src/test/java/com/generalmobi/smart/helmet/core/core/BootstrapApiClientUtilTest.java
+++ app/src/test/java/com/generalmobi/smart/helmet/core/core/BootstrapApiClientUtilTest.java
@@ -6,7 +6,7 @@ import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 import com.generalmobi.smart.finder.core.BootstrapService;
-import com.generalmobi.smart.finder.core.User;
+ 
 
 import java.util.List;
 
@@ -17,12 +17,5 @@ import org.junit.Test;
  * Unit tests of client API
  */
 public class BootstrapApiClientUtilTest {
-
-    @Test
-    @Ignore("Requires the API to use basic authentication. Parse.com api does not. See BootstrapService for more info.")
-    public void shouldCreateClient() throws Exception {
-        List<User> users = new BootstrapService("demo@androidbootstrap.com", "foobar").getUsers();
-
-        assertThat(users.get(0).getUsername(), notNullValue());
-    }
+ 
 }
--- app/src/test/java/com/generalmobi/smart/helmet/core/core/BootstrapServiceTest.java
+++ app/src/test/java/com/generalmobi/smart/helmet/core/core/BootstrapServiceTest.java
@@ -7,10 +7,6 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.doReturn;
 
 import com.generalmobi.smart.finder.core.BootstrapService;
-import com.generalmobi.smart.finder.core.CheckIn;
-import com.generalmobi.smart.finder.core.News;
-import com.generalmobi.smart.finder.core.User;
-import com.generalmobi.smart.finder.core.UserAgentProvider;
 import com.github.kevinsawicki.http.HttpRequest;
 
 import java.io.BufferedReader;
@@ -30,75 +26,5 @@ import org.mockito.runners.MockitoJUnitRunner;
  */
 @RunWith(MockitoJUnitRunner.class)
 public class BootstrapServiceTest {
-
-    /**
-     * Create reader for string
-     *
-     * @param value
-     * @return input stream reader
-     * @throws IOException
-     */
-    private static BufferedReader createReader(String value) throws IOException {
-        return new BufferedReader(new InputStreamReader(new ByteArrayInputStream(
-                value.getBytes(HttpRequest.CHARSET_UTF8))));
-    }
-
-    @Mock
-    private HttpRequest request;
-
-    private BootstrapService service;
-
-    /**
-     * Set up default mocks
-     *
-     * @throws IOException
-     */
-    @Before
-    public void before() throws IOException {
-        service = new BootstrapService("foo", new UserAgentProvider()) {
-            protected HttpRequest execute(HttpRequest request) throws IOException {
-                return BootstrapServiceTest.this.request;
-            }
-        };
-        doReturn(true).when(request).ok();
-    }
-
-    /**
-     * Verify getting users with an empty response
-     *
-     * @throws IOException
-     */
-    @Test
-    public void getUsersEmptyResponse() throws IOException {
-        doReturn(createReader("")).when(request).bufferedReader();
-        List<User> users = service.getUsers();
-        assertNotNull(users);
-        assertTrue(users.isEmpty());
-    }
-
-    /**
-     * Verify getting news with an empty response
-     *
-     * @throws IOException
-     */
-    @Test
-    public void getContentEmptyResponse() throws IOException {
-        doReturn(createReader("")).when(request).bufferedReader();
-        List<News> content = service.getNews();
-        assertNotNull(content);
-        assertTrue(content.isEmpty());
-    }
-
-    /**
-     * Verify getting checkins with an empty response
-     *
-     * @throws IOException
-     */
-    @Test
-    public void getReferrersEmptyResponse() throws IOException {
-        doReturn(createReader("")).when(request).bufferedReader();
-        List<CheckIn> referrers = service.getCheckIns();
-        assertNotNull(referrers);
-        assertTrue(referrers.isEmpty());
-    }
+ 
 }
