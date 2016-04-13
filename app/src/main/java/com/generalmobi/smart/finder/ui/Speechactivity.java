a99d9be671be878bae5afca644523744dd31d1de
abhishek <abhishek.dixit@generalmobi.com>
2016-04-13 14:18:28 +0530

    Flash Light Changes

--- app/src/main/java/com/generalmobi/smart/finder/ui/Speechactivity.java
+++ app/src/main/java/com/generalmobi/smart/finder/ui/Speechactivity.java
@@ -1,14 +1,26 @@
 package com.generalmobi.smart.finder.ui;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.TransitionDrawable;
+import android.hardware.Camera;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.speech.RecognizerIntent;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.Toast;
 
 import com.generalmobi.smart.finder.R;
 import com.generalmobi.smart.finder.util.ShakeListener;
+import com.generalmobi.smart.finder.util.ShakeService;
 
 import java.security.SecureRandom;
 import java.util.ArrayList;
@@ -20,6 +32,10 @@ import timber.log.Timber;
  * Created by admin on 4/12/2016.
  */
 public class Speechactivity extends BootstrapActivity{
+    public static Camera cam = null;
+    private boolean isFlashOn;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +48,39 @@ public class Speechactivity extends BootstrapActivity{
         startActivityForResult(intent, 1001);
 
     }
+    public void flashLightOn() {
+
+    try {
+        if (getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_CAMERA_FLASH)) {
+            cam = Camera.open();
+            Camera.Parameters p = cam.getParameters();
+            p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            cam.setParameters(p);
+            cam.startPreview();
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+        Toast.makeText(getBaseContext(), "Exception flashLightOn()",
+                Toast.LENGTH_SHORT).show();
+    }
+}
+    public void flashLightOff() {
+        try {
+            if (getPackageManager().hasSystemFeature(
+                    PackageManager.FEATURE_CAMERA_FLASH)) {
+                cam.stopPreview();
+                cam.release();
+                cam = null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getBaseContext(), "Exception flashLightOff",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -46,10 +95,24 @@ public class Speechactivity extends BootstrapActivity{
                     vibe.vibrate(1000);
                     MediaPlayer mp = MediaPlayer.create(this, R.raw.iamhere);
                     mp.start();
+                    for(int i=0;i<10;i++){
+                        flashLightOn();
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        flashLightOff();
+                    }
+
+
+                }
+
+
 
                 }
             }
         }
 
     }
-}
+
